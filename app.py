23be6bf4be0f59dd2c0be4d8dc3dc36f1bbff122
narayana676 <narayana955050@gmail.com>
2026-08-08 14:43:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -867,11 +867,3 @@ except Exception as e:
 # RUN APP
 # ============================================================
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
